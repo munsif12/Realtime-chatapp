6909e2b065fc384fde0ce266a1f4ee7a926e32bb
Munsif Ali Misri <53260498+munsif12@users.noreply.github.com>
2022-04-14 10:01:07 +0500

    signinSignupPage

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,7 +1,37 @@
 import React from 'react'
+import RegistrationForm from '../components/RegistrationForm';
+import LoginForm from '../components/LoginForm';
+import Overlay from '../components/Overlay';
+import useLoginRegStates from '../hooks/useLoginRegStates';
+
+
 const Home = () => {
+    const [
+        activeTab, setactiveTab, showPassword, regFormData, loginFormData, regFormErr, loginFormErr,
+        handleToggleShowPassword, handleChangeForBothLoginAndRegForm, submitBothLoginAndRegistrationForm
+    ] = useLoginRegStates();
+
+
     return (
-        <div>HOME</div>
+        <div className={`container ${activeTab} mainContainerLoginReg`} id='container'>
+            <RegistrationForm
+                showPassword={showPassword}
+                handleToggleShowPassword={handleToggleShowPassword}
+                regFormData={regFormData}
+                regFormErr={regFormErr}
+                handleChangeForBothLoginAndRegForm={handleChangeForBothLoginAndRegForm}
+                submitBothLoginAndRegistrationForm={submitBothLoginAndRegistrationForm}
+            />
+            <LoginForm
+                showPassword={showPassword}
+                handleToggleShowPassword={handleToggleShowPassword}
+                loginFormData={loginFormData}
+                loginFormErr={loginFormErr}
+                handleChangeForBothLoginAndRegForm={handleChangeForBothLoginAndRegForm}
+                submitBothLoginAndRegistrationForm={submitBothLoginAndRegistrationForm}
+            />
+            <Overlay setactiveTab={setactiveTab} />
+        </div>
     )
 }
 
