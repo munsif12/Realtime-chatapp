d8e9af98e358681c777eb3e753325bfba6b1665b
Munsif Ali Misri <53260498+munsif12@users.noreply.github.com>
2022-04-17 10:46:36 +0500

    helper function cloudnary image upload

---
+++ client/src/helpers/uploadImageToCloudnary.js
@@ -0,0 +1,13 @@
+export default async function uploadImageToCloudinary(CLOUDINARY_URL, data) {
+    try {
+        const resp = await fetch(CLOUDINARY_URL, {
+            method: 'POST',
+            body: data,
+        });
+        const { secure_url } = await resp.json();
+        return secure_url;
+    } catch (error) {
+        return error;
+    }
+
+} 
