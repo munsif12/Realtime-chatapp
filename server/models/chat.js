e5231cc90bfa8cae81af468aaa877b5a25d83f61
Munsif Ali Misri <53260498+munsif12@users.noreply.github.com>
2022-04-23 16:46:57 +0500

    chat and message model

--- server/models/chat.js
+++ server/models/chat.js
@@ -1,19 +1,16 @@
 const mongoose = require("mongoose");
-
-const chatModel = mongoose.Schema(
+const { Schema } = mongoose
+const chatModel = Schema(
     {
         chatName: { type: String, trim: true },
         isGroupChat: { type: Boolean, default: false },
-        users: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-        latestMessage: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Message",
-        },
-        groupAdmin: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+        users: [{ type: Schema.Types.ObjectId, ref: "user" }],
+        latestMessage: { type: Schema.Types.ObjectId, ref: "message" },
+        groupAdmin: { type: Schema.Types.ObjectId, ref: "user" },
     },
     { timestamps: true }
 );
 
-const Chat = mongoose.model("Chat", chatModel);
+const Chat = mongoose.model("chat", chatModel);
 
 module.exports = Chat;
---
+++ server/models/message.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+const messageSchema = Schema(
+    {
+        senderId: { type: Schema.Types.ObjectId, ref: "user" },
+        content: { type: String, trim: true },
+        chatId: { type: Schema.Types.ObjectId, ref: "chat" },
+        reciverId: [{ type: Schema.Types.ObjectId, ref: "user" }],
+    },
+    { timestamps: true }
+);
+
+const Message = mongoose.model("message", messageSchema);
+module.exports = Message;
