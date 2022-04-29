7e004aba1044248b2dee953ee533dabf7b7cf6bf
Munsif Ali Misri <53260498+munsif12@users.noreply.github.com>
2022-04-29 12:56:06 +0500

    axios-tosend-authToken-with-requests

--- client/src/config/axios.js
+++ client/src/config/axios.js
@@ -1,13 +1,14 @@
 import axios from "axios";
 axios.defaults.baseURL = process.env.REACT_APP_URL || "http://localhost:4000/api/";
-// const authToken = localStorage.getItem("authToken");
-// if (authToken) {
-//     axios.defaults.headers.common = {
-//         Authorization: `bearer ${authToken}`,
-//     };
-// } else {
-//     axios.defaults.headers.common = {
-//         Authorization: null,
-//     };
-// }
+const authToken = localStorage.getItem("authToken");
+console.log({ authToken })
+if (authToken) {
+    axios.defaults.headers.common = {
+        Authorization: `bearer ${authToken}`,
+    };
+} else {
+    axios.defaults.headers.common = {
+        Authorization: null,
+    };
+}
 export default axios;
