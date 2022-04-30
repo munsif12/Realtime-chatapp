150717b700acbaaac871002ef3d2dccde44c821b
Munsif Ali Misri <53260498+munsif12@users.noreply.github.com>
2022-04-30 15:37:23 +0500

    filter-chat and users

---
+++ client/src/helpers/filterSearchValue.js
@@ -0,0 +1,14 @@
+export function selectUserFilter(searchArray, searchKey) {
+    if (!searchArray) return [];
+    return searchArray.filter(chat => (
+        chat.name.toLowerCase().includes(searchKey.toLowerCase()) ||
+        chat.email.toLowerCase().includes(searchKey.toLowerCase())
+    ))
+}
+
+export function homeChatsFilter(searchArray, searchLey) {
+    if (!searchArray) return [];
+    return searchArray.filter(chat => chat.isGroupChat ?
+        chat.chatName.toLowerCase().includes(searchLey.toLowerCase()) :
+        chat.users.some(user => user.name.toLowerCase().includes(searchLey.toLowerCase())))
+}
