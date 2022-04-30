de6dee896396139ffc9698dc7e1f8954e05f3cb2
Munsif Ali Misri <53260498+munsif12@users.noreply.github.com>
2022-04-30 15:40:14 +0500

    rootReducer-chat+user

--- client/src/redux/index.js
+++ client/src/redux/index.js
@@ -1,10 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
 
 import authReducer from './slices/auth'
+import chatsReducer from './slices/chats'
+import usersReducer from './slices/users'
 
 const store = configureStore({
     reducer: {
         auth: authReducer,
+        chats: chatsReducer,
+        users: usersReducer,
     }
 })
 
